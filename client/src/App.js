ed9616877c9a0269c7106389ee32587c1e442948
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2023-06-02 18:01:00 +0530

    ..

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
 require("dotenv").config();
 
 const { MONGO_URL, PORT } = process.env;
@@ -20,16 +21,22 @@ app.listen(PORT,(req,res)=>{
 
 app.use(
     cors({
-      origin: ["http://localhost:4000"],
+      origin: ["http://localhost:3000"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
+      
     })
   );
   
 
+  app.use(cookieParser());
+  
   app.use(express.json()); 
 //   express.json(): The express.json() will add a body property 
 // to the request or req object. 
 // This includes the request body's parsed JSON data. 
 // req.body in your route handler function 
 // will allow you to access this data.
+
+
+app.use("/", authRoute);
